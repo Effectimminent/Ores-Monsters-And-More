74f2e72724a0d8da2edc3d59e044089def9507e9
BuildTools <unconfigured@null.spigotmc.org>
2024-01-09 05:01:56 -0600

    0.1.6

--- src/main/java/io/github/effectimminent/OmamItems.java
+++ src/main/java/io/github/effectimminent/OmamItems.java
@@ -3,6 +3,7 @@ package io.github.effectimminent;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.item.Item;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class OmamItems {
@@ -17,12 +18,9 @@ public class OmamItems {
     public static void registerRenders(){
         registerRender(copper_ingot);
     }
-    public static void registerRender(Item item)
-    {
+    public static void registerRender(Item item) {
         Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(item, 0, new ModelResourceLocation(Reference.MOD_ID + ":" + item.getUnlocalizedName().substring(5), "inventory"));
-
     }
-
     public static void registerRender(Item item, int meta, String fileName) {
         Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(item, meta, new ModelResourceLocation(Reference.MOD_ID + ":" + fileName, "inventory"));
     }
