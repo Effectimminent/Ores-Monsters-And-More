e101d7d08ada58d408f94bc8e3b81206f88993f6
BuildTools <unconfigured@null.spigotmc.org>
2024-01-11 08:15:31 -0600

    0.6.0 first potion on weapon

--- src/main/java/io/github/effectimminent/Items/ItemCopperSword.java
+++ src/main/java/io/github/effectimminent/Items/ItemCopperSword.java
@@ -1,9 +1,26 @@
 package io.github.effectimminent.Items;
 
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.world.World;
 
 public class ItemCopperSword extends ItemSword {
     public ItemCopperSword(ToolMaterial material) {
         super(material);
+    }
+    @Override
+    public boolean hitEntity(ItemStack stack, EntityLivingBase target, EntityLivingBase attacker) {
+        if (attacker instanceof EntityPlayer) {
+            EntityPlayer player = (EntityPlayer) attacker;
+            World world = player.getEntityWorld();
+
+            // Apply a potion effect to the target
+            target.addPotionEffect(new PotionEffect(19, 100, 1));
+            // Replace 'PotionTypes.POISON' with the desired potion effect
+        }
+        return super.hitEntity(stack, target, attacker);
     }
 }
