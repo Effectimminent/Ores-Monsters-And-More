ecbb9404a63e10ad47fe33a3a1d6c01698dd94c5
BuildTools <unconfigured@null.spigotmc.org>
2024-01-11 08:23:26 -0600

    0.6.1

--- src/main/java/io/github/effectimminent/Items/ItemCopperSword.java
+++ src/main/java/io/github/effectimminent/Items/ItemCopperSword.java
@@ -11,16 +11,5 @@ public class ItemCopperSword extends ItemSword {
     public ItemCopperSword(ToolMaterial material) {
         super(material);
     }
-    @Override
-    public boolean hitEntity(ItemStack stack, EntityLivingBase target, EntityLivingBase attacker) {
-        if (attacker instanceof EntityPlayer) {
-            EntityPlayer player = (EntityPlayer) attacker;
-            World world = player.getEntityWorld();
 
-            // Apply a potion effect to the target
-            target.addPotionEffect(new PotionEffect(19, 100, 1));
-            // Replace 'PotionTypes.POISON' with the desired potion effect
-        }
-        return super.hitEntity(stack, target, attacker);
-    }
 }
