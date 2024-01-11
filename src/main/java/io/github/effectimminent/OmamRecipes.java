0a4dcf956362e74dd5c8fba268484c74ce39a149
BuildTools <unconfigured@null.spigotmc.org>
2024-01-11 04:32:19 -0600

    0.5.2

--- src/main/java/io/github/effectimminent/OmamRecipes.java
+++ src/main/java/io/github/effectimminent/OmamRecipes.java
@@ -11,5 +11,15 @@ public class OmamRecipes {
         GameRegistry.addRecipe(new ItemStack(OmamItems.copper_axe),new Object[] {" cc", " sc"," s ", 'c' ,OmamItems.copper_ingot, 's', Items.stick });
         GameRegistry.addRecipe(new ItemStack(OmamItems.copper_axe),new Object[] {" cc", " cs"," s ", 'c' ,OmamItems.copper_ingot, 's', Items.stick });
         GameRegistry.addRecipe(new ItemStack(OmamItems.copper_axe),new Object[] {"cc ", "sc ","s  ", 'c' ,OmamItems.copper_ingot, 's', Items.stick });
+        GameRegistry.addRecipe(new ItemStack(OmamBlocks.copper_block),new Object[] {"ccc", "ccc","ccc", 'c' ,OmamItems.copper_ingot});
+        GameRegistry.addRecipe(new ItemStack(OmamItems.copper_pickaxe),new Object[] {"ccc", " s "," s ", 'c' ,OmamItems.copper_ingot, 's', Items.stick });
+        GameRegistry.addRecipe(new ItemStack(OmamItems.copper_hoe),new Object[] {"cc ", " s "," s ", 'c' ,OmamItems.copper_ingot, 's', Items.stick });
+        GameRegistry.addRecipe(new ItemStack(OmamItems.copper_hoe),new Object[] {" cc", " s "," s ", 'c' ,OmamItems.copper_ingot, 's', Items.stick });
+        GameRegistry.addRecipe(new ItemStack(OmamItems.copper_hoe),new Object[] {" cc", "  s"," s ", 'c' ,OmamItems.copper_ingot, 's', Items.stick });
+        GameRegistry.addRecipe(new ItemStack(OmamItems.copper_hoe),new Object[] {"cc ", "s  ","s  ", 'c' ,OmamItems.copper_ingot, 's', Items.stick });
+        GameRegistry.addRecipe(new ItemStack(OmamItems.copper_sword),new Object[] {"c ", "c ","s  ", 'c' ,OmamItems.copper_ingot, 's', Items.stick });
+        GameRegistry.addRecipe(new ItemStack(OmamItems.copper_sword),new Object[] {" c ", " c "," s  ", 'c' ,OmamItems.copper_ingot, 's', Items.stick });
+        GameRegistry.addRecipe(new ItemStack(OmamItems.copper_sword),new Object[] {"  c", "  c","  s", 'c' ,OmamItems.copper_ingot, 's', Items.stick });
+
     }
 }
