1ca386f3d9460f8839f1101b75820b0fb5715000
BuildTools <unconfigured@null.spigotmc.org>
2024-01-13 01:48:10 -0600

    0.7.4

--- src/main/java/io/github/effectimminent/OmamRecipes.java
+++ src/main/java/io/github/effectimminent/OmamRecipes.java
@@ -8,21 +8,21 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 public class OmamRecipes {
     public static void  register()
     {
-        GameRegistry.addRecipe(new ItemStack(OmamItems.lapis_axe),new Object[] {"cc ", "cs "," s ", 'c' ,Items.dye, EnumDyeColor.BLUE.getDyeDamage(), 's', Items.stick });
-        GameRegistry.addRecipe(new ItemStack(OmamItems.lapis_axe),new Object[] {" cc", " sc"," s ", 'c' ,Items.dye, EnumDyeColor.BLUE.getDyeDamage(), 's', Items.stick });
-        GameRegistry.addRecipe(new ItemStack(OmamItems.lapis_axe),new Object[] {" cc", " cs"," s ", 'c' ,Items.dye, EnumDyeColor.BLUE.getDyeDamage(), 's', Items.stick });
-        GameRegistry.addRecipe(new ItemStack(OmamItems.lapis_axe),new Object[] {"cc ", "sc ","s  ", 'c' ,Items.dye, EnumDyeColor.BLUE.getDyeDamage(), 's', Items.stick });
-        GameRegistry.addRecipe(new ItemStack(OmamItems.lapis_pickaxe),new Object[] {"ccc", " s "," s ", 'c' ,Items.dye, EnumDyeColor.BLUE.getDyeDamage(), 's', Items.stick });
-        GameRegistry.addRecipe(new ItemStack(OmamItems.lapis_hoe),new Object[] {"cc ", " s "," s ", 'c' ,Items.dye, EnumDyeColor.BLUE.getDyeDamage(), 's', Items.stick });
-        GameRegistry.addRecipe(new ItemStack(OmamItems.lapis_hoe),new Object[] {" cc", " s "," s ", 'c' ,Items.dye, EnumDyeColor.BLUE.getDyeDamage(), 's', Items.stick });
-        GameRegistry.addRecipe(new ItemStack(OmamItems.lapis_hoe),new Object[] {" cc", "  s","  s", 'c' ,Items.dye, EnumDyeColor.BLUE.getDyeDamage(), 's', Items.stick });
-        GameRegistry.addRecipe(new ItemStack(OmamItems.lapis_hoe),new Object[] {"cc ", "s  ","s  ", 'c' ,Items.dye, EnumDyeColor.BLUE.getDyeDamage(), 's', Items.stick });
-        GameRegistry.addRecipe(new ItemStack(OmamItems.lapis_sword),new Object[] {"c  ", "c  ","s  ", 'c' ,Items.dye, EnumDyeColor.BLUE.getDyeDamage(), 's', Items.stick });
-        GameRegistry.addRecipe(new ItemStack(OmamItems.lapis_sword),new Object[] {" c ", " c "," s ", 'c' ,Items.dye, EnumDyeColor.BLUE.getDyeDamage(), 's', Items.stick });
-        GameRegistry.addRecipe(new ItemStack(OmamItems.lapis_sword),new Object[] {"  c", "  c","  s", 'c' ,Items.dye, EnumDyeColor.BLUE.getDyeDamage(), 's', Items.stick });
-        GameRegistry.addRecipe(new ItemStack(OmamItems.lapis_shovel),new Object[] {"c  ", "s  ","s  ", 'c' ,Items.dye, EnumDyeColor.BLUE.getDyeDamage(), 's', Items.stick });
-        GameRegistry.addRecipe(new ItemStack(OmamItems.lapis_shovel),new Object[] {" c ", " s "," s ", 'c' ,Items.dye, EnumDyeColor.BLUE.getDyeDamage(), 's', Items.stick });
-        GameRegistry.addRecipe(new ItemStack(OmamItems.lapis_shovel),new Object[] {"  c", "  s","  s", 'c' ,Items.dye, EnumDyeColor.BLUE.getDyeDamage(), 's', Items.stick });
+        GameRegistry.addRecipe(new ItemStack(OmamItems.lapis_axe),new Object[] {"cc ", "cs "," s ", 'c' ,Items.dye,1 ,4, 's', Items.stick });
+        GameRegistry.addRecipe(new ItemStack(OmamItems.lapis_axe),new Object[] {" cc", " sc"," s ", 'c' ,Items.dye,1 ,4, 's', Items.stick });
+        GameRegistry.addRecipe(new ItemStack(OmamItems.lapis_axe),new Object[] {" cc", " cs"," s ", 'c' ,Items.dye,1 ,4, 's', Items.stick });
+        GameRegistry.addRecipe(new ItemStack(OmamItems.lapis_axe),new Object[] {"cc ", "sc ","s  ", 'c' ,Items.dye,1 ,4, 's', Items.stick });
+        GameRegistry.addRecipe(new ItemStack(OmamItems.lapis_pickaxe),new Object[] {"ccc", " s "," s ", 'c' ,Items.dye,1 ,4, 's', Items.stick });
+        GameRegistry.addRecipe(new ItemStack(OmamItems.lapis_hoe),new Object[] {"cc ", " s "," s ", 'c' ,Items.dye,1 ,4, 's', Items.stick });
+        GameRegistry.addRecipe(new ItemStack(OmamItems.lapis_hoe),new Object[] {" cc", " s "," s ", 'c' ,Items.dye,1 ,4, 's', Items.stick });
+        GameRegistry.addRecipe(new ItemStack(OmamItems.lapis_hoe),new Object[] {" cc", "  s","  s", 'c' ,Items.dye,1 ,4, 's', Items.stick });
+        GameRegistry.addRecipe(new ItemStack(OmamItems.lapis_hoe),new Object[] {"cc ", "s  ","s  ", 'c' ,Items.dye,1 ,4, 's', Items.stick });
+        GameRegistry.addRecipe(new ItemStack(OmamItems.lapis_sword),new Object[] {"c  ", "c  ","s  ", 'c' ,Items.dye,1 ,4, 's', Items.stick });
+        GameRegistry.addRecipe(new ItemStack(OmamItems.lapis_sword),new Object[] {" c ", " c "," s ", 'c' ,Items.dye,1 ,4, 's', Items.stick });
+        GameRegistry.addRecipe(new ItemStack(OmamItems.lapis_sword),new Object[] {"  c", "  c","  s", 'c' ,Items.dye,1 ,4, 's', Items.stick });
+        GameRegistry.addRecipe(new ItemStack(OmamItems.lapis_shovel),new Object[] {"c  ", "s  ","s  ", 'c' ,Items.dye,1 ,4, 's', Items.stick });
+        GameRegistry.addRecipe(new ItemStack(OmamItems.lapis_shovel),new Object[] {" c ", " s "," s ", 'c' ,Items.dye,1 ,4, 's', Items.stick });
+        GameRegistry.addRecipe(new ItemStack(OmamItems.lapis_shovel),new Object[] {"  c", "  s","  s", 'c' ,Items.dye,1 ,4, 's', Items.stick });
         GameRegistry.addRecipe(new ItemStack(OmamItems.emerald_axe),new Object[] {"cc ", "cs "," s ", 'c' ,Items.emerald, 's', Items.stick });
         GameRegistry.addRecipe(new ItemStack(OmamItems.emerald_axe),new Object[] {" cc", " sc"," s ", 'c' ,Items.emerald, 's', Items.stick });
         GameRegistry.addRecipe(new ItemStack(OmamItems.emerald_axe),new Object[] {" cc", " cs"," s ", 'c' ,Items.emerald, 's', Items.stick });
