4f774cd1a56c43944b108573356c7731e3f55ed3
BuildTools <unconfigured@null.spigotmc.org>
2024-01-12 23:54:18 -0600

    0.6.23

--- src/main/java/io/github/effectimminent/AutoArmorToolRecipe.java
+++ src/main/java/io/github/effectimminent/AutoArmorToolRecipe.java
@@ -11,29 +11,29 @@ public class AutoArmorToolRecipe {
 
     public static void addGear(String materialIn) {
         try {
-            Field field = OmamItems.class.getField(materialIn);
-            Field field0 = OmamItems.class.getField(materialIn + "_block");
-            Field field1 = OmamItems.class.getField(materialIn + "_sword");
-            Field field2 = OmamItems.class.getField(materialIn + "_pickaxe");
-            Field field3 = OmamItems.class.getField(materialIn + "_axe");
-            Field field4 = OmamItems.class.getField(materialIn + "_shovel");
-            Field field5 = OmamItems.class.getField(materialIn + "_hoe");
-            Field field6 = OmamItems.class.getField(materialIn + "_helmet");
-            Field field7 = OmamItems.class.getField(materialIn + "_chestplate");
-            Field field8 = OmamItems.class.getField(materialIn + "_leggings");
-            Field field9 = OmamItems.class.getField(materialIn + "_boots");
+            String ingotgemName =materialIn;
+            String blockName =materialIn + "_block";
+            String swordName =materialIn + "_sword";
+            String pickaxeName =materialIn + "_pickaxe";
+            String axeName =materialIn + "_axe";
+            String shovelName =materialIn + "_shovel";
+            String hoeName =materialIn + "_hoe";
+            String helmetName =materialIn + "_helmet";
+            String chestplateName =materialIn + "_chestplate";
+            String leggingsName =materialIn + "_leggings";
+            String bootsName =materialIn + "_boots";
 
-            Item ingotgem = (Item) field.get(null);
-            Item block = (Item) field0.get(null);
-            Item sword = (Item) field1.get(null);
-            Item pickaxe = (Item) field2.get(null);
-            Item axe = (Item) field3.get(null);
-            Item shovel = (Item) field4.get(null);
-            Item hoe = (Item) field5.get(null);
-            Item helmet = (Item) field6.get(null);
-            Item chestplate = (Item) field7.get(null);
-            Item leggings = (Item) field8.get(null);
-            Item boots = (Item) field9.get(null);
+            Item ingotgem = (Item) OmamItems.class.getField(ingotgemName).get(null);
+            Item block = (Item) OmamItems.class.getField(blockName).get(null);
+            Item sword = (Item) OmamItems.class.getField(swordName).get(null);
+            Item pickaxe = (Item) OmamItems.class.getField(pickaxeName).get(null);
+            Item axe = (Item) OmamItems.class.getField(axeName).get(null);
+            Item shovel = (Item) OmamItems.class.getField(shovelName).get(null);
+            Item hoe = (Item) OmamItems.class.getField(hoeName).get(null);
+            Item helmet = (Item) OmamItems.class.getField(helmetName).get(null);
+            Item chestplate = (Item) OmamItems.class.getField(chestplateName).get(null);
+            Item leggings = (Item) OmamItems.class.getField(leggingsName).get(null);
+            Item boots = (Item) OmamItems.class.getField(bootsName).get(null);
 
             // Use 'sword' as needed...
             GameRegistry.addRecipe(new ItemStack(axe),new Object[] {"cc ", "cs "," s ", 'c' ,ingotgem, 's', Items.stick });
