a742df610cb25c7852155a4eec7405fff534a4d1
BuildTools <unconfigured@null.spigotmc.org>
2024-01-14 04:45:20 -0600

    0.8.15

--- src/main/java/io/github/effectimminent/worldgen/OreGen.java
+++ src/main/java/io/github/effectimminent/worldgen/OreGen.java
@@ -2,7 +2,10 @@ package io.github.effectimminent.worldgen;
 
 import java.util.Random;
 
+import com.google.common.base.Predicate;
 import io.github.effectimminent.OmamBlocks;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
 import net.minecraft.util.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
@@ -47,7 +50,8 @@ public class OreGen implements IWorldGenerator {
          topazGenerator   = new WorldGenMinable(OmamBlocks.topaz_ore.getDefaultState(), 10);
          zirconGenerator  = new WorldGenMinable(OmamBlocks.zircon_ore.getDefaultState(), 10);
          sapphireGenerator= new WorldGenMinable(OmamBlocks.sapphire_ore.getDefaultState(), 10);
-         amethystGenerator= new WorldGenMinable(OmamBlocks.amethyst_ore.getDefaultState(), 24);
+        amethystGenerator = new WorldGenMinable(OmamBlocks.amethyst_ore.getDefaultState(), 10);
+
 
     }
 
