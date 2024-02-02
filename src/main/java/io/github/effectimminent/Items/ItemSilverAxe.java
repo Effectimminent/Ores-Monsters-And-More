2e984ce003cdaed175829c5e3e4024a5c1e22425
BuildTools <unconfigured@null.spigotmc.org>
2024-02-02 06:59:52 -0600

    1.2.5
    1.1.2 is fine
    1.1.3-1.1.7 should be 1.2.0-1.2.4

--- src/main/java/io/github/effectimminent/Items/ItemSilverAxe.java
+++ src/main/java/io/github/effectimminent/Items/ItemSilverAxe.java
@@ -1,9 +1,22 @@
 package io.github.effectimminent.Items;
 
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.entity.Entity;
 import net.minecraft.item.ItemAxe;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 
 public class ItemSilverAxe extends ItemAxe {
     public ItemSilverAxe(ToolMaterial material) {
         super(material);
+    }
+    @Override
+    public void onUpdate(ItemStack stack, World world, Entity entity, int i, boolean bool) {
+        super.onUpdate(stack, world, entity, i, bool);
+
+        if (!stack.isItemEnchanted()) {
+            stack.addEnchantment(Enchantment.smite, 5);
+            // Replace 'yourEnchantment' with the actual enchantment you want to apply
+        }
     }
 }
--- src/main/java/io/github/effectimminent/Items/ItemSilverSword.java
+++ src/main/java/io/github/effectimminent/Items/ItemSilverSword.java
@@ -1,10 +1,23 @@
 package io.github.effectimminent.Items;
 
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.entity.Entity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
+import net.minecraft.world.World;
 
 public class ItemSilverSword extends ItemSword {
     public ItemSilverSword(ToolMaterial material) {
         super(material);
+    }
+    @Override
+    public void onUpdate(ItemStack stack, World world, Entity entity, int i, boolean bool) {
+        super.onUpdate(stack, world, entity, i, bool);
+
+        if (!stack.isItemEnchanted()) {
+            stack.addEnchantment(Enchantment.smite, 5);
+            // Replace 'yourEnchantment' with the actual enchantment you want to apply
+        }
     }
 
 }
