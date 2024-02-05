df402091847287d4c0f48244197ba54f6437d3b8
BuildTools <unconfigured@null.spigotmc.org>
2024-02-05 08:49:32 -0600

    1.3.3

--- src/main/java/io/github/effectimminent/Reference.java
+++ src/main/java/io/github/effectimminent/Reference.java
@@ -3,7 +3,7 @@ package io.github.effectimminent;
 public class Reference {
     public static final String  MOD_ID = "Omam";
     public static final String  MOD_NAME = "OresMonstersAndMore";
-    public static final String  VERSION = "1.3.0";
+    public static final String  VERSION = "1.3.3";
     public static final String  CLIENT_PROXY_CLASS ="io.github.effectimminent.proxy.ClientProxy";
     public static final String  SERVER_PROXY_CLASS ="io.github.effectimminent.proxy.CommonProxy";
 }
