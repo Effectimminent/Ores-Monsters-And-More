b904fd8fafd7c72a855536158afe9d091d0aee4b
BuildTools <unconfigured@null.spigotmc.org>
2024-04-04 19:20:32 -0500

    1.3.9

--- src/main/java/io/github/effectimminent/Items/ItemRubySword.java
+++ src/main/java/io/github/effectimminent/Items/ItemRubySword.java
@@ -1,9 +1,26 @@
 package io.github.effectimminent.Items;
 
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.world.World;
 
 public class ItemRubySword extends ItemSword {
     public ItemRubySword(ToolMaterial material) {
         super(material);
+    }
+    @Override
+    public boolean hitEntity(ItemStack stack, EntityLivingBase target, EntityLivingBase attacker) {
+        if (attacker instanceof EntityPlayer) {
+            EntityPlayer player = (EntityPlayer) attacker;
+            World world = player.getEntityWorld();
+
+            // Apply a potion effect to the target
+            attacker.addPotionEffect(new PotionEffect(5, 100, 0));
+            // Replace 'PotionTypes.POISON' with the desired potion effect
+        }
+        return super.hitEntity(stack, target, attacker);
     }
 }
